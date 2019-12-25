79dfcd47acb0ee6e823370bf624ecfd54dae1fbf
mikkou <developer.0112358@gmail.com>
2019-12-26 01:03:55 +0300

    minor fix

--- bot/app/models/User.js
+++ bot/app/models/User.js
@@ -6,8 +6,8 @@ const UserSchema = new Schema(
     id: { type: Number, required: true, trim: true },
     is_bot: { type: Boolean, required: true },
     first_name: { type: String, required: true, trim: true },
-    last_name: { type: String, required: true, trim: true },
-    username: { type: String, required: true, trim: true },
+    last_name: { type: String, default: null, trim: true },
+    username: { type: String, default: null, trim: true },
     language_code: { type: String, required: true, trim: true },
     last_word_id: { type: String, trim: true, default: null, lowercase: true },
     createdAt: { type: Date, default: Date.now }
